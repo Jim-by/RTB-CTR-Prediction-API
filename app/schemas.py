7d1050be6aff6fac5ff0416d9783557b8e8155b1
Uladzimir Manulenka <vlma@tut.by>
2025-12-30 14:38:10 +0300

    Update schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,20 @@
+from pydantic import BaseModel
+from typing import Optional
 
+class PredictionRequest(BaseModel):
+    hour: int
+    banner_pos: int
+    site_id: str
+    site_domain: str
+    site_category: str
+    app_id: str
+    app_domain: str
+    app_category: str
+    device_id: str
+    device_type: int
+    device_conn_type: int
+    C1: Optional[int] = None
+
+class PredictionResponse(BaseModel):
+    predicted_ctr: float
+    is_weekend: bool
